8872946cbba5d717e5ef7e32cb5d7bf539ac79a9
Mehedi Mostafa Hafiz <mehedimostafabisd@gmail.com>
2026-02-18 14:43:20 +0000

    Update test_repo.py

--- test_repo.py
+++ test_repo.py
@@ -3,3 +3,12 @@ from models.task import Task
 from datetime import datetime
 
 repo = TaskRepository()
+
+task = Task(
+    title="Deep Work",
+    priority="HIGH",
+    energy_required="HIGH",
+    deadline=datetime(2026, 2, 10, 18, 0),
+    estimated_duration=90
+)
+repo.create_task(task)
